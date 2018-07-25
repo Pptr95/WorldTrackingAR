a6f4e44a55273f83babfa26b1ade7ee6a6861e00
Petru Potrimba <petru.potrimba@studio.unibo.it>
2018-07-25 11:31:26 +0200

    import repo

---
+++ WorldTracking/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  WorldTracking
+//
+//  Created by Valerio Potrimba on 25/07/2018.
+//  Copyright © 2018 Petru Potrimba. All rights reserved.
+//
+
+import UIKit
+import ARKit
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    @IBAction func add(_ sender: UIButton) {
+        let node = SCNNode()
+        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+        node.position = SCNVector3(0, 0, 0)
+        self.sceneView.scene.rootNode.addChildNode(node)
+    }
+    let configuration = ARWorldTrackingConfiguration()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.session.run(configuration)
+    }
+
+
+}
+
