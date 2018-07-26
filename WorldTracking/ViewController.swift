5a1d904dd2096368353ba98f5d2065976b91cbd1
Petru Potrimba <petru.potrimba@studio.unibo.it>
2018-07-26 14:27:04 +0200

    add rotation feature!

--- WorldTracking/ViewController.swift
+++ WorldTracking/ViewController.swift
@@ -16,6 +16,16 @@ class ViewController: UIViewController {
         self.resetSession()
     }
     @IBAction func add(_ sender: UIButton) {
+        /*let node = SCNNode(geometry: SCNCylinder(radius: 0.1, height: 0.2))
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        node.position = SCNVector3(0.0, 0.0, -0.3)
+        node.eulerAngles = SCNVector3(-90.degreesToRadiants, 0, 0)
+        
+        let pyramidNode = SCNNode(geometry: SCNPyramid(width: 0.1, height: 0.1, length: 0.1))
+        pyramidNode.geometry?.firstMaterial?.diffuse.contents = UIColor.green
+        pyramidNode.position = SCNVector3(0.0, 0.0, -0.3)
+        self.sceneView.scene.rootNode.addChildNode(node)
+        node.addChildNode(pyramidNode)*/
         let node = SCNNode()
         let doorNode = SCNNode(geometry: SCNPlane(width: 0.03, height: 0.06))
         doorNode.geometry?.firstMaterial?.diffuse.contents = UIColor.white
@@ -23,18 +33,19 @@ class ViewController: UIViewController {
         let boxNode = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0))
         boxNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
         boxNode.position = SCNVector3(0.0, -0.05, 0.0)
-       /*let path = UIBezierPath()
+        let path = UIBezierPath()
         path.move(to: CGPoint(x: 0.0, y: 0.0))
         path.addLine(to: CGPoint(x: 0.0, y: 0.2))
         path.addLine(to: CGPoint(x: 0.2, y: 0.3))
         path.addLine(to: CGPoint(x: 0.4, y: 0.2))
         path.addLine(to: CGPoint(x: 0.4, y: 0.0))
         let shape = SCNShape(path: path, extrusionDepth: 0.1)
-        node.geometry = shape*/
+        node.geometry = shape
         node.geometry = SCNPyramid(width: 0.1, height: 0.1, length: 0.1)
         node.geometry?.firstMaterial?.specular.contents = UIColor.white
         node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
         node.position = SCNVector3(0.2, 0.2, 0.2)
+        node.eulerAngles = SCNVector3(Float(180.degreesToRadiants), 0, 0)
         self.sceneView.scene.rootNode.addChildNode(node)
         node.addChildNode(boxNode)
         boxNode.addChildNode(doorNode)
@@ -60,6 +71,9 @@ class ViewController: UIViewController {
     func randomNumbers(firstNum: CGFloat, secondNum: CGFloat) ->CGFloat {
         return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNum - secondNum) + min(firstNum, secondNum)
     }
+}
 
+extension Int {
+    var degreesToRadiants: Double { return Double(self) * Double.pi/180 }
 }
 
