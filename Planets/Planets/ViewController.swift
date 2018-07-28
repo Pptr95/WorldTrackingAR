790dfe170ed1ce43999050b78888df09c96a4483
Petru Potrimba <petru.potrimba@studio.unibo.it>
2018-07-28 14:17:17 +0200

    complete Planets and create new project Whack a jellyfish

--- Planets/Planets/ViewController.swift
+++ Planets/Planets/ViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Valerio Potrimba on 26/07/2018.
 //  Copyright © 2018 Petru Potrimba. All rights reserved.
 //
-
+/*
 import UIKit
 import ARKit
 class ViewController: UIViewController {
@@ -42,6 +42,100 @@ class ViewController: UIViewController {
 
 }
 
+
 extension Int {
     var degreesToRadians: Double { return Double(self) * Double.pi/180}
 }
+*/
+import UIKit
+import ARKit
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+        self.sceneView.session.run(configuration)
+        self.sceneView.autoenablesDefaultLighting = true
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let sun = SCNNode(geometry: SCNSphere(radius: 0.35))
+        let earthParent = SCNNode()
+        let venusParent = SCNNode()
+        let moonParent = SCNNode()
+        
+        /*This doesn't work in XCode 10
+        sun.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "Sun diffuse")
+        */
+        sun.geometry?.firstMaterial?.diffuse.contents = UIColor.gray
+        sun.position = SCNVector3(0,0,-1)
+        earthParent.position = SCNVector3(0,0,-1)
+        venusParent.position = SCNVector3(0,0,-1)
+        moonParent.position = SCNVector3(1.2 ,0 , 0)
+        
+        self.sceneView.scene.rootNode.addChildNode(sun)
+        self.sceneView.scene.rootNode.addChildNode(earthParent)
+        self.sceneView.scene.rootNode.addChildNode(venusParent)
+        
+        
+        let earth = planet(geometry: SCNSphere(radius: 0.2), diffuse: UIColor.blue, position: SCNVector3(1.2 ,0 , 0))
+        let venus = planet(geometry: SCNSphere(radius: 0.1), diffuse: UIColor.green, position: SCNVector3(0.7, 0, 0))
+        let moon = planet(geometry: SCNSphere(radius: 0.05), diffuse: UIColor.white, position: SCNVector3(0,0,-0.3))
+        
+        
+        
+        let sunAction = self.rotation(time: 8)
+        let earthParentRotation = self.rotation(time: 14)
+        let venusParentRotation = self.rotation(time: 10)
+        let earthRotation = self.rotation(time: 8)
+        let moonRotation = self.rotation(time: 5)
+        let venusRotation = self.rotation(time: 8)
+        
+        
+        earth.runAction(earthRotation)
+        venus.runAction(venusRotation)
+        earthParent.runAction(earthParentRotation)
+        venusParent.runAction(venusParentRotation)
+        moonParent.runAction(moonRotation)
+        
+        
+        sun.runAction(sunAction)
+        earthParent.addChildNode(earth)
+        earthParent.addChildNode(moonParent)
+        venusParent.addChildNode(venus)
+        earth.addChildNode(moon)
+        moonParent.addChildNode(moon)
+        
+    }
+    
+    func planet(geometry: SCNGeometry, diffuse: UIColor, position: SCNVector3) -> SCNNode {
+        let planet = SCNNode(geometry: geometry)
+        planet.geometry?.firstMaterial?.diffuse.contents = diffuse
+        planet.position = position
+        return planet
+        
+    }
+    
+    func rotation(time: TimeInterval) -> SCNAction {
+        let rotationTime = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: time)
+        let foreverRotation = SCNAction.repeatForever(rotationTime)
+        return foreverRotation
+    }
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+extension Int {
+    var degreesToRadians: Double { return Double(self) * .pi/180}
+}
+
---
+++ Whack a jellyfish/Whack a jellyfish/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Whack a jellyfish
+//
+//  Created by Valerio Potrimba on 27/07/2018.
+//  Copyright © 2018 Petru Potrimba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
