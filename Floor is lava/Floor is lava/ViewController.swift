b32c9cfa2e025b9cc267851f52a9cf4926dd5356
Petru Potrimba <petru.potrimba@studio.unibo.it>
2018-07-28 18:41:35 +0200

    implement plane detection

--- Floor is lava/Floor is lava/ViewController.swift
+++ Floor is lava/Floor is lava/ViewController.swift
@@ -22,18 +22,39 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         self.sceneView.delegate = self
     }
 
+    func createLava(for planeAnchor: ARPlaneAnchor) -> SCNNode {
+        let lavaNode = SCNNode()
+        lavaNode.geometry = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
+        lavaNode.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        lavaNode.geometry?.firstMaterial?.isDoubleSided = true
+        lavaNode.position = SCNVector3(planeAnchor.center.x, planeAnchor.center.y, planeAnchor.center.z)
+        lavaNode.eulerAngles = SCNVector3(CGFloat(90.deegresToRadiants), 0.0, 0.0)
+        return lavaNode
+    }
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
+        let lavaNode = createLava(for: planeAnchor)
+        node.addChildNode(lavaNode)
         print("new plane")
     }
     
     func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
         guard let planeAnchor = anchor as? ARPlaneAnchor else {return}
         print("update floor's anchor...")
+        node.enumerateChildNodes { (childNode, _) in
+            childNode.removeFromParentNode()
+        }
+        let lavaNode = createLava(for: planeAnchor)
+        node.addChildNode(lavaNode)
     }
-    
     func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
-        <#code#>
+        guard let _ = anchor as? ARPlaneAnchor else {return}
+        node.enumerateChildNodes { (childNode, _) in
+            childNode.removeFromParentNode()
+        }
     }
+}
+extension Int {
+    var deegresToRadiants: Double { return Double(self) * .pi/180}
 }
 
